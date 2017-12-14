3735445d8ac67d52e02bdfaf18b60b4b136a6a0f
Alizadeh Pegah <alizadeh@douglas.info.unicaen.fr>
2017-12-14 14:33:22 +0100

    another version

--- manage_ami.py
+++ manage_ami.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import re
 
 class manage_meeting:
 
@@ -62,28 +63,87 @@ class manage_meeting:
 
         return (words_files, segments_file)
 
-    def get_roots(self):
+    def get_roots_words(self):
 
-        (word_list, segment_list) = self.get_files()
+        tempo = self.get_files()
+        word_list = tempo[0]
 
-        participants = [item for item in self.get_participants().iterkeys()]
-        word_roots = segment_roots = dict.fromkeys(participants)
+        self.participants = [item for item in self.get_participants().iterkeys()]
+        word_roots = dict.fromkeys(self.participants)
 
-        for item in range(len(participants)):
-            tree = ET.parse(word_list[item])
-            word_roots[participants[item]] = tree.getroot()
+        for item in range(len(self.participants)):
+            tree_ = ET.parse(word_list[item])
+            word_roots[self.participants[item]] = tree_.getroot()
+
+        print word_roots['A'].attrib
+
+        return word_roots
+
+    def get_roots_segments(self):
+
+        tempo = self.get_files()
+        segment_roots = dict.fromkeys(self.participants)
+
+        segment_list = tempo[1]
+
+        for item in range(len(self.participants)):
             tree = ET.parse(segment_list[item])
-            segment_roots[participants[item]] = tree.getroot()
+            segment_roots[self.participants[item]] = tree.getroot()
 
-        return (word_roots, segment_roots)
+        print segment_roots['A'].attrib
+
+        return segment_roots
 
     def generate_text(self):
 
-        (word_roots, segment_roots) = self.get_roots()
-        
+        # a minimum timer among all participants
+        total_time_pointer = 10000000.0
+        total_speaker_pointer = None
 
+        word_roots = self.get_roots_words()
+        print 'MMMMMMMMMM', word_roots['A'].attrib
+        segment_roots = self.get_roots_segments()
+        print 'MMMMMMMMMM', segment_roots['A'].attrib
+
+        print  segment_roots
+        print word_roots
+
+        #timer on each participants that shows which time of speaker we are analysing at the moment
+        time_pointer_on_participants = {i: float(segment_roots[i][0].attrib['transcriber_start']) for i in self.participants}
+        print time_pointer_on_participants
+
+        #a couneter on XML childeren that indicates where we are during the dialogue transcription
+        counter_on_segment_xml = {i:0 for i in self.participants}
+
+        for key,value in time_pointer_on_participants.iteritems():
+             if value < total_time_pointer:
+                total_time_pointer = value
+                total_speaker_pointer = key
+
+        #update time poniter on total time pointer
+        time_pointer_on_participants[total_speaker_pointer] = float(segment_roots[total_speaker_pointer][0].attrib['transcriber_end'])
+
+        print total_speaker_pointer, total_time_pointer
+        print counter_on_segment_xml
+
+        tempo = segment_roots[total_speaker_pointer][counter_on_segment_xml[total_speaker_pointer]][0].attrib['href']
+        (start_word, stop_word) = self.get_words_interval_for_speaker(tempo)
+
+        print start_word
+        print stop_word
+
+        print word_roots['C'].attrib
 
         return
+
+    #takes the href in xml segmentation and separates the start and finish words
+    def get_words_interval_for_speaker(self, tempo):
+
+        start_word = tempo[(tempo.index('(') + len('(')):tempo.index(')')]
+        tempo = tempo[tempo.index(')') + 1:]
+        finish_word = tempo[(tempo.index('..id(') + len('..id(')):tempo.index(')')]
+
+        return (start_word, finish_word)
 
 
 ex = manage_meeting("MAN", "meet_1")
@@ -91,7 +151,7 @@ ex.initializations()
 ex.get_meeting_list()
 #ex.get_participants('meet_1')
 
-ex.get_roots()
+ex.generate_text()
 
 
 
